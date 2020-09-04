2a201322d1a7152411d815039e0434225cb2a762
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2020-09-05 01:48:02 +0900

    refactor: dev

--- apps/next-ec-app/pages/_app.tsx
+++ apps/next-ec-app/pages/_app.tsx
@@ -1,9 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AppProps } from 'next/app';
 import './styles.css';
+import {
+  Box,
+  Button,
+  Collapsible,
+  Grommet,
+  Header,
+  Heading,
+  Sidebar,
+} from 'grommet';
+import { Cart } from 'grommet-icons';
+
+const theme = {
+  global: {
+    font: {
+      family: 'Roboto',
+      size: '18px',
+      height: '20px',
+    },
+  },
+};
 
 const CustomApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  const [isVisibleCart, setIsVisibleCart] = useState(false);
+  return (
+    <Grommet theme={theme} full>
+      <Box fill>
+        <Header
+          direction={'row'}
+          background={'dark-2'}
+          pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+          elevation={'medium'}
+          style={{ zIndex: 1 }}
+        >
+          <Heading level={'3'} margin={'none'}>
+            EC App
+          </Heading>
+          <Button
+            icon={<Cart />}
+            onClick={() => setIsVisibleCart(!isVisibleCart)}
+          />
+        </Header>
+        <Box direction={'row'} flex overflow={{ horizontal: 'hidden' }}>
+          <Component {...pageProps} />
+          <Collapsible direction={'horizontal'} open={isVisibleCart}>
+            <Sidebar
+              width={'medium'}
+              background={'light-2'}
+              align={'center'}
+              justify={'center'}
+            >
+              Sidebar
+            </Sidebar>
+          </Collapsible>
+        </Box>
+      </Box>
+    </Grommet>
+  );
 };
 
 export default CustomApp;
--- apps/next-ec-app/pages/index.tsx
+++ apps/next-ec-app/pages/index.tsx
@@ -1,30 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import fetch from 'node-fetch';
 
-import {
-  Box,
-  Button,
-  Card,
-  Collapsible,
-  Grid,
-  Grommet,
-  Header,
-  Heading,
-  Sidebar,
-} from 'grommet';
-import { Cart } from 'grommet-icons';
+import { Box, Card, Grid, Heading, Sidebar } from 'grommet';
 import { GetServerSideProps } from 'next';
 import { Product } from '@nx-vercel-workspace/types';
-
-const theme = {
-  global: {
-    font: {
-      family: 'Roboto',
-      size: '18px',
-      height: '20px',
-    },
-  },
-};
 
 const basePath = `http://localhost:3333`;
 export const getServerSideProps: GetServerSideProps = async () => {
@@ -40,7 +19,6 @@ interface IndexPageProps {
 }
 
 export const Index = ({ products }: IndexPageProps) => {
-  const [isVisibleCart, setIsVisibleCart] = useState(false);
   const brands = [
     'BEAMS',
     'SHIPS',
@@ -50,69 +28,40 @@ export const Index = ({ products }: IndexPageProps) => {
     'URBAN RESEARCH',
   ];
   return (
-    <Grommet theme={theme} full>
-      <Box fill>
-        <Header
-          direction={'row'}
-          background={'dark-2'}
-          pad={{ left: 'medium', right: 'small', vertical: 'small' }}
-          elevation={'medium'}
-          style={{ zIndex: 1 }}
-        >
-          <Heading level={'3'} margin={'none'}>
-            EC App
-          </Heading>
-          <Button
-            icon={<Cart />}
-            onClick={() => setIsVisibleCart(!isVisibleCart)}
-          />
-        </Header>
-        <Box direction={'row'} flex overflow={{ horizontal: 'hidden' }}>
-          <Sidebar
-            width={'small'}
-            background={'light-2'}
-            align={'center'}
-            justify={'center'}
-          >
-            Sidebar
-          </Sidebar>
-          <Box flex pad={'large'}>
-            <Heading level={'4'}>New Arrival</Heading>
-            <Grid columns={{ count: 3, size: 'auto' }} gap={'small'}>
-              {products.map((product) => (
-                <Card key={product.id} pad={'large'}>
-                  {product.name}
-                </Card>
-              ))}
-            </Grid>
-            <Heading level={'4'}>Brands</Heading>
-            <Grid columns={{ count: 6, size: 'auto' }} gap={'small'}>
-              {brands.map((brand, i) => (
-                <Box
-                  align={'center'}
-                  justify={'center'}
-                  background={'light-3'}
-                  key={i}
-                  pad={'large'}
-                >
-                  {brand}
-                </Box>
-              ))}
-            </Grid>
-          </Box>
-          <Collapsible direction={'horizontal'} open={isVisibleCart}>
-            <Sidebar
-              width={'medium'}
-              background={'light-2'}
+    <>
+      <Sidebar
+        width={'small'}
+        background={'light-2'}
+        align={'center'}
+        justify={'center'}
+      >
+        Sidebar
+      </Sidebar>
+      <Box flex pad={'large'}>
+        <Heading level={'4'}>New Arrival</Heading>
+        <Grid columns={{ count: 3, size: 'auto' }} gap={'small'}>
+          {products.map((product) => (
+            <Card key={product.id} pad={'large'}>
+              {product.name}
+            </Card>
+          ))}
+        </Grid>
+        <Heading level={'4'}>Brands</Heading>
+        <Grid columns={{ count: 6, size: 'auto' }} gap={'small'}>
+          {brands.map((brand, i) => (
+            <Box
               align={'center'}
               justify={'center'}
+              background={'light-3'}
+              key={i}
+              pad={'large'}
             >
-              Sidebar
-            </Sidebar>
-          </Collapsible>
-        </Box>
+              {brand}
+            </Box>
+          ))}
+        </Grid>
       </Box>
-    </Grommet>
+    </>
   );
 };
 
